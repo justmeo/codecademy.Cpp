1e338460b8ec3ca906f785da5c119c6773c3e5a2
justmeo <mrabeti.yassine@gmail.com>
2025-06-12 15:08:24 +0400

    done

---
+++ HelloWorld/initials.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << " SSS   L\n";
+    std::cout << "S   S  L\n";
+    std::cout << "S      L\n";
+    std::cout << " SSS   L\n";
+    std::cout << "    S  L\n";
+    std::cout << "S   S  L\n";
+    std::cout << " SSS   LLLLLL\n";
+
+}
